4bde89f2a9ea9cf2e78258cc6de678a783da8ea1
Rahul Shaw <rahul.shaw.274@gmail.com>
2019-12-31 02:21:52 +0530

    added queue

--- Queue/app-wll.py
+++ Queue/app-wll.py
@@ -3,40 +3,41 @@ class Node():
         self.value = value
         self.next = None
 
-class Stack():
+class Queue():
     def __init__(self):
         self.head = None
         self.tail = None
         self.len = 0
 
-    def push(self, value):
-        print('Pushed value ' + str(value) + ' into the stack.')
+    def enQueue(self, value):
+        print('Pushed value ' + str(value) + ' into the queue.')
         newNode = Node(value)
         if(not self.head):
             self.head = newNode
             self.tail = newNode
         else:
-            newNode.next = self.head
-            self.head = newNode
+            self.tail.next = newNode
+            self.tail = newNode
 
-    def pop(self):
+    def deQueue(self):
         if(not self.head):
-            print('The stack is empty')
+            print('The quque is empty')
             return
-        print('poped value ' + str(self.head.value) + ' from the stack')
+        print('dequeued value ' + str(self.head.value) + ' from the queue')
         self.head = self.head.next
         if(not self.head):
+            self.head = None
             self.tail = None
 
     def peek(self):
         if(self.head):
             print('The value on top is ' + str(self.head.value))
         else:
-            print('the stack is empty')
-
+            print('the queue is empty')
+    
     def print(self):
         temp = self.head
-        print('\ncurrent Stack')
+        print('\ncurrent Queue')
         print('####################')
         while temp is not None:
             print(temp.value)
@@ -52,23 +53,22 @@ class Stack():
         print('length: ', self.len)
         print('####################\n')
 
-if __name__=='__main__': 
-    st = Stack()
-    st.push(10)
-    st.push(20)
-    st.push(30)
-    st.push(40)
-    st.push(50)
-    st.peek()
-    st.pop()
-    st.peek()
-    # st.pop()
-    # st.print()
-    # st.pop()
-    # st.print()
-    # st.pop()
-    # st.print()
-    # st.pop()
-    # st.print()
-    # st.pop()
-    # st.print()
+if __name__ == "__main__":
+    qu = Queue()
+    qu.enQueue(10)
+    qu.enQueue(20)
+    qu.enQueue(30)
+    qu.enQueue(40)
+    qu.enQueue(50)
+    qu.deQueue()
+    qu.peek()
+    qu.deQueue()
+    qu.peek()
+    qu.deQueue()
+    qu.peek()
+    qu.deQueue()
+    qu.peek()
+    qu.deQueue()
+    qu.peek()
+    qu.deQueue()
+    qu.peek()

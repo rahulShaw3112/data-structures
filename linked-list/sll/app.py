cfc4ef63ee64d77f29b09a0c5e449da0895b9590
Rahul Shaw <rahul.shaw.274@gmail.com>
2019-12-30 15:36:39 +0530

    added tail

--- linked-list/sll/app.py
+++ linked-list/sll/app.py
@@ -6,6 +6,7 @@ class Node():
 class Sll():
     def __init__(self): 
         self.head = None
+        self.tail = None
         self.len = 0
     
     def insert(self, value, index):
@@ -13,15 +14,26 @@ class Sll():
             print('Not possible')
             return
         newNode = Node(value)
-        #insert at first position
-        if(index == 0):
+        # insert with no node inserted.
+        if(not self.head):
+            self.head = newNode
+            self.tail = newNode
+            
+        # insert at beginning of list.
+        elif(index == 0): 
             newNode.next = self.head
             self.head = newNode
-        #insert in betweeen
+
+        # insert at end of list.
+        elif(index == self.len):
+            self.tail.next = newNode
+            self.tail = newNode
+
+        #insert in between
         else:
             temp = self.traverseTill(index)
             newNode.next = temp.next
-            temp.next = newNode
+            temp.next = newNode 
         self.len += 1
         print('node inserted with value '+ str(newNode.value) + ' at index ' + str(index))
     
@@ -30,17 +42,27 @@ class Sll():
             print('Not possible')
             return
         temp = self.head
+
+        if(self.len == 1):
+            self.head = None
+            self.tail = None
+        
         #for first node
-        if(index == 0):
+        elif(index == 0):
             print('deleted node with value ' + str(self.head.value) + ' at index ' + str(index))
             self.head = self.head.next
+            
+        #for in between and last
         else:
-            #for in between
             temp = self.traverseTill(index)
             print('deleted node with value ' + str(temp.next.value) + ' at index ' + str(index))
             temp.next = temp.next.next
-        self.len -= 1
-    
+            # if last element
+            if(index == (self.len - 1)):
+                self.tail = temp
+
+        self.len -= 1 
+        
     def traverseTill(self, index):
         i = 0
         temp = self.head
@@ -72,6 +94,10 @@ class Sll():
             print('\nhead: ' + str(self.head.value))
         else:
             print('\nhead: none')
+        if(self.tail):
+            print('tail: ' + str(self.tail.value))
+        else:
+            print('tail: none')
         print('length: ', self.len)
         print('####################\n')
 
@@ -80,13 +106,16 @@ if __name__=='__main__':
     ll.insert(10,0)
     ll.insert(20,1)
     ll.insert(30,2)
-    # ll.insert(40,3)
-    # ll.insert(50,5)
-    # ll.delete(-1)
-    ll.delete(0)
-    ll.delete(0)
-    ll.delete(0)
-    ll.delete(0)
-    # ll.delete(1)
+    ll.insert(40,3)
+    ll.insert(50,4)
     ll.print()
+    ll.delete(0)
+    ll.print()
+    ll.delete(3)
+    ll.print()
+    ll.delete(1)
+    ll.print()
+    ll.delete(1)
+    ll.print()
+    ll.delete(0)
     ll.print()
